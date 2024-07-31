19a744c5cab2139236d99771e8ae74899a39693a
ikalangita <rahajason@gmail.com>
2024-07-31 08:52:46 +0200

    login form zod

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,7 +1,35 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod"
+
 import { LoginForm } from "@/components/auth/login-form";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+
+import { LoginSchema } from "@/schemas"
+
 const LoginPage = () => {
+
+    const form = useForm<z.infer<typeof LoginSchema>>(
+        {
+            resolver: zodResolver(LoginSchema),
+            defaultValues: {
+                email: "",
+                password: ""
+            }
+        }
+    );
+
     return(
         <CardWrapper 
             headerLabel="Welcom back" 
