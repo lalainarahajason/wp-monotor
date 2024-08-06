a5a39a747d79b31f673e2b2f6bf4719a093ee9ac
ikalangita <rahajason@gmail.com>
2024-08-06 08:26:24 +0200

    fix : replace bcrypt by bcryptjs

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,5 @@
 import type { NextAuthConfig } from "next-auth";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import Credentials from "next-auth/providers/credentials"
 import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
